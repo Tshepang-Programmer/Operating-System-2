3fb46c76c6ff2595c3d230e9a82ff42dad527c95
Tshepang <145541441+Tshepang-Programmer@users.noreply.github.com>
2023-10-27 11:50:23 +0200

    Update Task2.sh

--- Task2.sh
+++ Task2.sh
@@ -26,6 +26,8 @@ nano websrv.sh
 echo "Checking if nginx is installed"
 sleep 4
 
+#!/bin/bash
+
 if ! which nginx > /dev/null 2>&1; then
   echo "Nginx not installed"
 
@@ -34,4 +36,49 @@ sudo apt-get -y install nginx
 sleep 4
 fi
 echo "Nginx already installed"
+echo "Checking Status" 
+
+systemctl is-active nginx
+sleep 1
+
+echo "Starting NGINX"
+sudo systemctl start nginx
+
+systemctl is-active nginx
+sleep 1
+
+if (systemctl is-active nginx)
+then 
+
+echo "Creating lab directory"
+sleep 4
+sudo mkdir /var/www/html/labs html
+
+
+echo "Changing user ownership"
+sleep 4
+sudo chown $USER:root /var/www/html/labs
+
+echo "Creating symbolic link to html"
+sleep 4
+
+sudo ln -s /var/www/html/labs html
+
+echo "Change working  directory"
+sleep 4
+cd html
+
+echo "Creating html file"
+sleep 4
+sudo touch index.html
+
+#echo "Opening index"
+
+#sleep 3
+#nano index.html
+
+else 
+echo "Error NGINX is not active"
+exit 1
+fi
 
