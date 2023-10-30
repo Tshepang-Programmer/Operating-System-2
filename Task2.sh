1ed89643f9ec4ad366a31d64fd7088b7d850021b
Tshepang <145541441+Tshepang-Programmer@users.noreply.github.com>
2023-10-30 16:30:30 +0200

    Update Task2.sh

--- Task2.sh
+++ Task2.sh
@@ -61,21 +61,21 @@ sudo touch index.html
 
 echo "Adding student information to html file"
 
-sudo echo "<!DOCTYPE html>" >> index.html
-sudo echo "<html lang="en">" >> index.html
-sudo echo "<head>" >> index.html
-sudo echo "  <meta charset="UTF-8">" >> index.html
-sudo echo "    <meta name="viewport" content="width=device-width, initial-scale=1.0">" >> index.html
-sudo echo "    <title>Document</title>" >> index.html
-sudo echo "</head>" >> index.html
-sudo echo "<body>" >> index.html
-sudo echo "    <h1> STUDENT INFORMATION PAGE </h1> " >> index.html
-sudo echo "     <P>" >> index.html
-sudo echo "                    Student 1" >> index.html
-sudo echo "        Initials:  TMD" >> index.html
-sudo echo "        Names: Tshepang Mugova David Letsoalo" >> index.html
-sudo echo "        Student no: 218575912" >> index.html
-sudo echo "        Course: Electrical Engineering : Computer Systems" >> index.html
+sudo echo " <!DOCTYPE html> " >>index.html
+sudo echo "<html lang="en">" >>index.html
+sudo echo "<head>" >>index.html
+sudo echo "  <meta charset="UTF-8">" >>index.html
+sudo echo "    <meta name="viewport" content="width=device-width, initial-scale=1.0">" >>index.html
+sudo echo "    <title>Document</title>" >>index.html
+sudo echo "</head>" >>index.html
+sudo echo "<body>" >>index.html
+sudo echo "    <h1> STUDENT INFORMATION PAGE </h1> " >>index.html
+sudo echo "     <P>" >>index.html
+sudo echo "                    Student 1" >>index.html
+sudo echo "        Initials:  TMD" >>index.html
+sudo echo "        Names: Tshepang Mugova David Letsoalo" >>index.html
+sudo echo "        Student no: 218575912" >>index.html
+sudo echo "        Course: Electrical Engineering : Computer Systems" >>index.html
 sudo echo "  " >> index.html
 sudo echo "  " >> index.html
 sudo echo "                   Student 2" >> index.html
