8ad1ae363a1ee6a2ca550dd7891e423e267bb7ca
Tshepang <145541441+Tshepang-Programmer@users.noreply.github.com>
2023-10-30 16:38:14 +0200

    Update Task2.sh

--- Task2.sh
+++ Task2.sh
@@ -51,49 +51,19 @@ sleep 4
 
 sudo ln -s /var/www/html/labs html
 
-echo "Change working  directory"
+echo "Changing  working  directory"
 sleep 4
 cd html
 
 echo "Creating html file"
-sleep 4
+sleep 2
 sudo touch index.html
 
-echo "Adding student information to html file"
+ sleep 
 
-sudo echo " <!DOCTYPE html> " >>index.html
-sudo echo "<html lang="en">" >>index.html
-sudo echo "<head>" >>index.html
-sudo echo "  <meta charset="UTF-8">" >>index.html
-sudo echo "    <meta name="viewport" content="width=device-width, initial-scale=1.0">" >>index.html
-sudo echo "    <title>Document</title>" >>index.html
-sudo echo "</head>" >>index.html
-sudo echo "<body>" >>index.html
-sudo echo "    <h1> STUDENT INFORMATION PAGE </h1> " >>index.html
-sudo echo "     <P>" >>index.html
-sudo echo "                    Student 1" >>index.html
-sudo echo "        Initials:  TMD" >>index.html
-sudo echo "        Names: Tshepang Mugova David Letsoalo" >>index.html
-sudo echo "        Student no: 218575912" >>index.html
-sudo echo "        Course: Electrical Engineering : Computer Systems" >>index.html
-sudo echo "  " >> index.html
-sudo echo "  " >> index.html
-sudo echo "                   Student 2" >> index.html
-sudo echo "        Initials:  D" >> index.html
-sudo echo "        Names: Dimpho Mosito" >> index.html
-sudo echo "        Student no: 222516860" >> index.html
-sudo echo "        Course: Electrical Engineering : Computer Systems" >> index.html
-sudo echo "  " >> index.html
-sudo echo "    </P>" >> index.html
-sudo echo "</body>" >> index.html
-sudo echo "</html>" >> index.html
+ echo "Opening index.html"
 
-echo "Information has been added to index.html"
-sleep 4
-echo "Viewing index.html"
-sleep 4
-
-cat index.html
+ sudo nano index.html
 
 
 else 
