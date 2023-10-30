fbd4fee1edc499f32b542dc52ec6e71e641649f1
Tshepang <145541441+Tshepang-Programmer@users.noreply.github.com>
2023-10-30 07:03:22 +0200

    Update testingscript.sh
    additional things outside the testing of the script

--- testingscript.sh
+++ testingscript.sh
@@ -9,3 +9,20 @@ echo "Creating script webserv.sh"
 touch webserv.sh
 
 sudo chmod u+x
+
+
+#!/bin/bash 
+sudo apt-get update
+sudo apt-get -y install nginx
+systemctl is active nginx
+sudo systemctl start nginx
+mkdir /var/www/html/labs html
+chown $USER:root /var/www/html/labs
+ln -s /var/www/html/labs html
+cd html
+touch index.html
+echo "<html><body><h1>Hello world</h1></body><html>" >> index.html
+cd 
+touch websrv.sh
+chmod u+x websrv.sh
+nano websrv.sh
