74efdb210f3be472473661bcca89c617dcd8c46a
Tshepang <145541441+Tshepang-Programmer@users.noreply.github.com>
2023-10-30 06:57:23 +0200

    Update Task2.sh

--- Task2.sh
+++ Task2.sh
@@ -1,20 +1,3 @@
-#!/bin/bash 
-
-sudo apt-get update
-sudo apt-get -y install nginx
-systemctl is active nginx
-sudo systemctl start nginx
-
-mkdir /var/www/html/labs html
-
-chown $USER:root /var/www/html/labs
-
-ln -s /var/www/html/labs html
-
-cd html
-
-touch index.html
-
 echo "<html><body><h1>Hello world</h1></body><html>" >> index.html
 
 cd 
