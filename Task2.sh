d9465aa0c82cc5a24e73ccabca2e33cdc03020e0
Tshepang <145541441+Tshepang-Programmer@users.noreply.github.com>
2023-10-30 15:57:30 +0200

    Update Task2.sh

--- Task2.sh
+++ Task2.sh
@@ -1,3 +1,4 @@
+#!/bin/bash 
 # TMD Letssoalo 218575912 & D Mosito 222516860
 
 echo "Updating"
@@ -60,35 +61,35 @@ sudo touch index.html
 
 echo "Adding student information to html file"
 
-echo "<!DOCTYPE html>" >> index.html
-echo "<html lang="en">" >> index.html
-echo "<head>" >> index.html
-echo "  <meta charset="UTF-8">" >> index.html
-echo "    <meta name="viewport" content="width=device-width, initial-scale=1.0">" >> index.html
-echo "    <title>Document</title>" >> index.html
-echo "</head>" >> index.html
-echo "<body>" >> index.html
-echo "    <h1> STUDENT INFORMATION PAGE </h1> " >> index.html
-echo "     <P>" >> index.html
-echo "                    Student 1" >> index.html
-echo "        Initials:  TMD" >> index.html
-echo "        Names: Tshepang Mugova David Letsoalo" >> index.html
-echo "        Student no: 218575912" >> index.html
-echo "        Course: Electrical Engineering : Computer Systems" >> index.html
-echo "  " >> index.html
-echo "  " >> index.html
-echo "                   Student 2" >> index.html
-echo "        Initials:  D" >> index.html
-echo "        Names: Dimpho Mosito" >> index.html
-echo "        Student no: 222516860" >> index.html
-echo "        Course: Electrical Engineering : Computer Systems" >> index.html
-echo "  " >> index.html
-echo "    </P>" >> index.html
-echo "</body>" >> index.html
-echo "</html>" >> index.html
+sudo echo "<!DOCTYPE html>" >> index.html
+sudo echo "<html lang="en">" >> index.html
+sudo echo "<head>" >> index.html
+sudo echo "  <meta charset="UTF-8">" >> index.html
+sudo echo "    <meta name="viewport" content="width=device-width, initial-scale=1.0">" >> index.html
+sudo echo "    <title>Document</title>" >> index.html
+sudo echo "</head>" >> index.html
+sudo echo "<body>" >> index.html
+sudo echo "    <h1> STUDENT INFORMATION PAGE </h1> " >> index.html
+sudo echo "     <P>" >> index.html
+sudo echo "                    Student 1" >> index.html
+sudo echo "        Initials:  TMD" >> index.html
+sudo echo "        Names: Tshepang Mugova David Letsoalo" >> index.html
+sudo echo "        Student no: 218575912" >> index.html
+sudo echo "        Course: Electrical Engineering : Computer Systems" >> index.html
+sudo echo "  " >> index.html
+sudo echo "  " >> index.html
+sudo echo "                   Student 2" >> index.html
+sudo echo "        Initials:  D" >> index.html
+sudo echo "        Names: Dimpho Mosito" >> index.html
+sudo echo "        Student no: 222516860" >> index.html
+sudo echo "        Course: Electrical Engineering : Computer Systems" >> index.html
+sudo echo "  " >> index.html
+sudo echo "    </P>" >> index.html
+sudo echo "</body>" >> index.html
+sudo echo "</html>" >> index.html
 
 echo "Information has been added to index.html"
-sleep 
+sleep 4
 echo "Viewing index.html"
 sleep 4
 
@@ -99,4 +100,3 @@ else
 echo "Error NGINX is not active"
 exit 1
 fi
-
