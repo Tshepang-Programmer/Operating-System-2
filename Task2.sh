bfe0208222eb28fcbfffe020739a0a1c4309e225
Tshepang <145541441+Tshepang-Programmer@users.noreply.github.com>
2023-10-31 20:57:35 +0200

    Update Task2.sh

--- Task2.sh
+++ Task2.sh
@@ -58,16 +58,55 @@ cd /home/azureuser/html/labs
 echo "Creating html file"
 sleep 2
 sudo touch index.html
-
  sleep 4
 
- echo "Opening index.html"
+ cd 
+ cd html 
+ cd labs
 
+echo "Adding student information to html file"
 sleep 4
- sudo nano index.html
+sudo echo " <!DOCTYPE html> " >>index.html
+sudo echo "<html lang="en">" >>index.html
+sudo echo "<head>" >>index.html
+sudo echo "  <meta charset="UTF-8">" >>index.html
+sudo echo "    <meta name="viewport" content="width=device-width, initial-scale=1.0">" >>index.html
+sudo echo "    <title>Document</title>" >>index.html
+sudo echo "</head>" >>index.html
+sudo echo "<body>" >>index.html
+sudo echo "    <h1> STUDENT INFORMATION PAGE </h1> " >>index.html
+sudo echo "     <P>" >>index.html
+sudo echo "                    Student 1" >>index.html
+sudo echo "        Initials:  TMD" >>index.html
+sudo echo "        Names: Tshepang Mugova David Letsoalo" >>index.html
+sudo echo "        Student no: 218575912" >>index.html
+sudo echo "        Course: Electrical Engineering : Computer Systems" >>index.html
+sudo echo "  " >> index.html
+sudo echo "  " >> index.html
+sudo echo "                   Student 2" >> index.html
+sudo echo "        Initials:  D" >> index.html
+sudo echo "        Names: Dimpho Mosito" >> index.html
+sudo echo "        Student no: 222516860" >> index.html
+sudo echo "        Course: Electrical Engineering : Computer Systems" >> index.html
+sudo echo "  " >> index.html
+sudo echo "    </P>" >> index.html
+sudo echo "</body>" >> index.html
+sudo echo "</html>" >> index.html
 
-sleep 4 
-cd /home/azureuser/html/labs
+echo "Information has been added to index.html"
+sleep 4
+echo "Viewing index.html"
+sleep 4
+
+cat index.html
+
+ #echo "Opening index.html"
+
+#sleep 4
+# sudo nano index.html
+
+#sleep 4 
+#cd /home/azureuser/html/labs
 else 
 echo "Error NGINX is not active"
 exit 1
